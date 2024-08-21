b37802663d6c7522baaac31bf224f2b8751001fc
olivebishop <olivehendrilgen1@gmail.com>
2024-08-21 14:43:00 +0300

    fix: tabs alignment

---
+++ app/(tabs)/orders.tsx
@@ -0,0 +1,12 @@
+import { View, Text,  } from 'react-native'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>Orders Screen</Text>
+    </View>
+  )
+}
+
+export default Page
--- app/(tabs)/wishlist.tsx
+++ app/(tabs)/wishlist.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const FavoriteScreen: React.FC = () => {
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text className='text-2xl font-bold'>Favorites</Text>
+      <Text className=''>Wishlists</Text>
     </View>
   );
 };
